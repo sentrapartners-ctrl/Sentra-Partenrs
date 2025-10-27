706753f4ae649f8e90b1f123b2777f80a2e404fc
Sentra Partners System <sentrapartners@system.com>
2025-10-27 19:05:30 -0400

    fix: Corrigir import trpc no password-reset-router

--- server/password-reset-router.ts
+++ server/password-reset-router.ts
@@ -1,4 +1,4 @@
-import { router, publicProcedure } from './trpc';
+import { router, publicProcedure } from './_core/trpc';
 import { z } from 'zod';
 import { getDb } from './db';
 import { users } from '../drizzle/schema';
