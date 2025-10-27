a03d546bdf563203dc7148717633f7bccc3697ff
Sentra Partners System <sentrapartners@system.com>
2025-10-27 18:00:24 -0400

    fix: Aumentar tamanho do modal de relatório de contas

--- client/src/components/AccountReportDialog.tsx
+++ client/src/components/AccountReportDialog.tsx
@@ -51,7 +51,7 @@ export function AccountReportDialog({ account, open, onOpenChange }: AccountRepo
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto">
+      <DialogContent className="max-w-7xl max-h-[95vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle className="text-2xl">
             Relatório Completo - {account.broker} #{account.accountNumber}
