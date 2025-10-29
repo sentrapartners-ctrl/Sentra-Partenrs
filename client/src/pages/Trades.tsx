4023061d16d886958fa0ca2d5b9125c70bf6fc8f
sentrapartners-ctrl <sentrapartners@gmail.com>
2025-10-29 05:25:53 -0400

    Ajustar divisão de lucro/prejuízo para 1.000.000 (Fase 2)

--- client/src/pages/Trades.tsx
+++ client/src/pages/Trades.tsx
@@ -182,7 +182,7 @@ export default function Trades() {
                         </span>
                         <span className="font-bold">
                           <InlineCurrencyValue 
-                            value={(trade.profit || 0) / 10000}
+                            value={(trade.profit || 0) / 1000000}
                             colored={true}
                           />
                         </span>
