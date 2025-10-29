155c79b2d3f6aeb72ca2c0cfbc02ba31e709f005
sentrapartners-ctrl <sentrapartners@gmail.com>
2025-10-29 05:09:46 -0400

    Remover conversão - exibir valor puro do profit

--- client/src/pages/Trades.tsx
+++ client/src/pages/Trades.tsx
@@ -182,7 +182,7 @@ export default function Trades() {
                         </span>
                         <span className="font-bold">
                           <InlineCurrencyValue 
-                            value={(trade.profit || 0) / 10000}
+                            value={trade.profit || 0}
                             colored={true}
                           />
                         </span>
